3d5ab914a2fd1e2b65e914aca001aa5fcb880e87
diegomoca <morales.castillo.diego@gmail.com>
2026-03-21 02:00:20 +0100

    create interface to load multiple instances of object

--- test.py
+++ test.py
@@ -1,38 +1,249 @@
+import threading
 import subprocess
 import time
+from rich.prompt import Prompt
+from rich.console import Console
 from osc4py3 import oscbuildparse
 from osc4py3.as_eventloop import *
+from osc4py3 import oscmethod as osm # needed to receive OSC
 
-# subprocess.Popen(['sclang', 'event-test.scd'])
+
+import instruments.harpa.source.frequenciestest as harpa
+
+
+console = Console()
+
+class Input(Prompt):
+    prompt_suffix = ": "
+    default_console = console
+
+console.rule('[bold]WELLCOME')
+
+instrList = ['harpa']
+commands = ['create', 'destroy', 'keyboardActivate', 'keyboardDeactivate', 'ccActivate', 'ccDeactivate', 'instrument']
+names = []
+instrDict = {}
+# set() is list without duplicates and order
+registered_addresses = set()
+
+def invalidNames():
+    return instrList + commands + names
+
+# function to calculate frequencies once instrument was successfully
+# created in supercollider
+created_flag = [False]
+def instFreqsCalc(address, msg):
+    instanceAddress = address
+    instanceName = instanceAddress.split('/')[-1]
+    flag = msg
+
+    if flag == 'created':
+        instrDict[instanceName] = {
+            'freqsCalc': harpa.Frequencies().startup(instanceAddress + '/freqs'),
+            'keyboardActivated': False,
+            'ccActivated': False,
+            'instanceAddress': instanceAddress
+        }
+        names.append(instanceName)
+        created_flag[0] = True
+
+
+# function to determin wwhether osc connection with instruments was successful
+instruments_ready_flag = [False]
+instruments_confirm_flag = [False]
+def instrumentsConfirmation(msg):
+    print(msg)
+    if msg == 'confirmed':
+        instruments_confirm_flag[0] = True
+    if msg == 'ready':
+        instruments_ready_flag[0] = True
+    
+    
+finished = False
+# define background event loop to process osc
+def oscEventLoop():
+    while not finished:
+        osc_process()
+        time.sleep(0.001)
+    osc_terminate()
+    print('exited event loop')
+
 
 # start OSC 
 osc_startup()
+# set up OSC server to receive messages
+osc_udp_server("127.0.0.1", 13131, 'scOSCServer')
+print("OSC Server started. Ready to receive messages!")
+# start background osc event loop
+threading.Thread(target=oscEventLoop).start()
+
+# run supercollider script
+subprocess.Popen(['sclang', 'event-test.scd'])
+
+# osc confirmation from supercollider
+osc_method('/instruments', instrumentsConfirmation)
+while not instruments_ready_flag[0]:
+    time.sleep(0.01)
 # set up client to send messages
-osc_udp_client('127.0.0.1', 57120, 'scOSCClient')
+osc_udp_client('127.0.0.1', 13130, 'scOSCClient')
+osc_send(oscbuildparse.OSCMessage('/instruments', ',s', ['ready']), 'scOSCClient')
+# osc reconfirmation from supercollider
+while not instruments_confirm_flag[0]:
+    time.sleep(0.01)
 print("OSC Client started. Ready to send messages!")
+console.print(f'[green]󰼁 OSC connection successfull![/green]\n')
 
-try:
-    while True:
-        oscMSG = input().split()
-        if oscMSG[0] == 'address':
-            oscAddr = oscMSG[1]
-            if len(oscMSG) > 2:
-                oscComm = oscMSG[2:]
-        elif oscAddr:
-            oscComm = oscMSG
-        else:
-            print('provide OSC address')
+exit_flag = [False]
+while True:
+    if exit_flag[0]:
+        userInput = ['exit']
+    else:
+        userInput = Input.ask('󰼂', choices=(names + ['create', 'exit']), console=console).split()
+    match userInput[0]:
+        case 'create':
+            if len(userInput) == 1:
+                userInput = Input.ask('[blue]󰼂 instrument[/blue]', choices=(instrList + ['home', 'exit']), console=console).split()
+                if userInput[0] == 'exit':
+                    exit_flag[0] = True
+                    continue
+                elif userInput[0] == 'home':
+                    continue
+                for instrument in instrList:
+                    if userInput[0] == instrument and len(userInput) == 1:
+                        userInput = Input.ask('[blue]󰼂 name[/blue]', console=console).split()
+                        if userInput[0] == 'exit':
+                            exit_flag[0] = True
+                            break
+                        elif userInput[0] == 'home':
+                            continue
+                        while len(userInput) != 1: 
+                            console.print('[red] no spaces allowed![/red]')
+                            userInput = Input.ask('[blue]󰼂 name[/blue]', console=console).split()
+                        for name in invalidNames():
+                            while userInput[0] == name:
+                                console.print('[red] invalid name![/red]')
+                                userInput = Input.ask('[blue]󰼂 name[/blue]', console=console).split()
+                        
+                        # create instrument
+                        # build osc address and osc message and send it so supercollider
+                        instanceName = userInput[0]
+                        classAddress = '/instruments/' + str(instrument)
+                        oscMessage = ['create', instanceName]
+                        # receive message from supercollider indicating that a new instance was created
+                        # and execute python script to calculate and send frequencies
+                        instanceAddress = classAddress + f'/{instanceName}'
+                        # because osc activated functions/addresses can't be removed they have to be
+                        # called and registered only once per session to avoid duplicate receivers
+                        if instanceAddress not in registered_addresses:
+                            osc_method(instanceAddress, instFreqsCalc, osm.OSCARG_ADDRESS + osm.OSCARG_DATAUNPACK)
+                            registered_addresses.add(instanceAddress)
+                        # send an OSC message to create a new instance of the instrument in supercollider
+                        osc_send(oscbuildparse.OSCMessage(classAddress, ',ss', oscMessage), 'scOSCClient')
+                        # wait for confirmation from supercollider
+                        while not created_flag[0]:
+                            time.sleep(0.01)
+                        created_flag[0] = False
+                        console.print(f'[green]󰼁 {instanceName} created![/green]\n')
 
-        print(oscAddr)
-        print(oscComm)
+        case name if name in names:
+            if len(userInput) == 1:
+                userInput = Input.ask(f'[blue]󰼂 {name}[/blue]', choices=['keyboard', 'cc', 'destroy', 'home', 'exit'], console=console).split()
+                if userInput[0] == 'exit':
+                    exit_flag[0] = True
+                    continue
+                elif userInput[0] == 'home':
+                    continue
+                match userInput[0]:
+                    case 'keyboard':
+                        address = instrDict[name]['instanceAddress'] + '/keyboard'
+                        if instrDict[name]['keyboardActivated'] == False:
+                            userInput = Input.ask(f'[blue]󰼂 {name} [bold]\[keyboard][/bold] (deactivated)[/blue]', choices=['activate', 'home', 'exit'], console=console).split()
+                            if userInput[0] == 'exit':
+                                exit_flag[0] = True
+                                continue
+                            elif userInput[0] == 'home':
+                                continue
+                            if len(userInput) == 1 and userInput[0] == 'activate':
+                                osc_send(oscbuildparse.OSCMessage(address, ',s', ['activate']), 'scOSCClient')
+                                # TODO make it so that it needs confirmation from supercollider to execute
+                                instrDict[name]['keyboardActivated'] = True
+                                console.print(f'[green]󰼁 {name} [bold]\[keyboard][/green][/bold]: [cyan2 italic]activated\n')
+                        elif instrDict[name]['keyboardActivated'] == True:
+                            userInput = Input.ask(f'[blue]󰼂 {name} [bold]\[keyboard][/bold] (activated)[/blue]', choices=['deactivate', 'home', 'exit'], console=console).split()
+                            if userInput[0] == 'exit':
+                                exit_flag[0] = True
+                                continue
+                            elif userInput[0] == 'home':
+                                continue
+                            if len(userInput) == 1 and userInput[0] == 'deactivate':
+                                osc_send(oscbuildparse.OSCMessage(address, ',s', ['deactivate']), 'scOSCClient')
+                                # TODO make it so that it needs confirmation from supercollider to execute
+                                instrDict[name]['keyboardActivated'] = False
+                                console.print(f'[green]󰼁 {name} [bold]\[keyboard][/green][/bold]: [orange1 italic]deactivated\n')
+                    case 'cc':
+                        address = instrDict[name]['instanceAddress'] + '/cc'
+                        if instrDict[name]['ccActivated'] == False:
+                            userInput = Input.ask(f'[blue]󰼂 {name} [bold]\[cc][/bold] (deactivated)[/blue]', choices=['activate', 'home', 'exit'], console=console).split()
+                            if userInput[0] == 'exit':
+                                exit_flag[0] = True
+                                continue
+                            elif userInput[0] == 'home':
+                                continue
+                            if len(userInput) == 1 and userInput[0] == 'activate':
+                                osc_send(oscbuildparse.OSCMessage(address, ',s', ['activate']), 'scOSCClient')
+                                # TODO make it so that it needs confirmation from supercollider to execute
+                                instrDict[name]['ccActivated'] = True
+                                console.print(f'[green]󰼁 {name} [bold]\[cc][/green][/bold]: [cyan2 italic]activated\n')
+                        elif instrDict[name]['ccActivated'] == True:
+                            userInput = Input.ask(f'[blue]󰼂 {name} [bold]\[cc][/bold] (activated)[/blue]', choices=['deactivate', 'home', 'exit'], console=console).split()
+                            if userInput[0] == 'exit':
+                                exit_flag[0] = True
+                                continue
+                            elif userInput[0] == 'home':
+                                continue
+                            if len(userInput) == 1 and userInput[0] == 'deactivate':
+                                osc_send(oscbuildparse.OSCMessage(address, ',s', ['deactivate']), 'scOSCClient')
+                                # TODO make it so that it needs confirmation from supercollider to execute
+                                instrDict[name]['ccActivated'] = False
+                                console.print(f'[green]󰼁 {name} [bold]\[cc][/green][/bold]: [orange1 italic]deactivated\n')
+                    case 'destroy':
+                        address = instrDict[name]['instanceAddress']
+                        if len(userInput) == 1:
+                            osc_send(oscbuildparse.OSCMessage(address, ',s', ['destroy']), 'scOSCClient')
+                            # TODO make it so that it needs confirmation from supercollider to execute
+                            names.remove(name)
+                            del instrDict[name]
 
-        osc_send(oscbuildparse.OSCMessage(oscAddr, None, oscComm), 'scOSCClient')
+        case 'exit':
+            osc_send(oscbuildparse.OSCMessage('/instruments', ',s', ['kill']), 'scOSCClient')
+            time.sleep(1.0)
+            finished = True
+            # TODO add confirmation from supercollider
+            break
 
-        for x in range(10):
-            osc_process()
-            time.sleep(0.001)
-except KeyboardInterrupt:
-    osc_terminate()
+
+# try:
+#     while True:
+#         userInput = input('󰼂 ').split() # 󰼂󰼁
+#         if userInput[0] == 'address':
+#             oscAddr = userInput[1]
+#             if len(userInput) > 2:
+#                 oscComm = userInput[2:]
+#         elif oscAddr:
+#             oscComm = userInput
+#         else:
+#             print('provide OSC address')
+
+#         print(oscAddr)
+#         print(oscComm)
+
+#         osc_send(oscbuildparse.OSCMessage(oscAddr, None, oscComm), 'scOSCClient')
+
+#         for x in range(10):
+#             osc_process()
+#             time.sleep(0.001)
+# except KeyboardInterrupt:
+#     osc_terminate()
 
 
 
---
+++ test2.py
@@ -0,0 +1,83 @@
+import threading
+import time
+from osc4py3.as_eventloop import * # needed to send and receive OSC
+from osc4py3 import oscbuildparse
+
+
+import instruments.harpa.source.frequenciestest as harpa
+
+finished = False
+# define background event loop to process osc
+def oscEventLoop():
+    while not finished:
+        osc_process()
+        time.sleep(0.001)
+    osc_terminate()
+    print('exited event loop')
+
+# start OSC 
+osc_startup()
+# set up client to send messages
+osc_udp_client('127.0.0.1', 57120, 'scOSCClient')
+print("OSC Client started. Ready to send messages!")
+# set up OSC server to receive messages
+osc_udp_server("127.0.0.1", 57121, 'scOSCServer')
+print("OSC Server started. Ready to receive messages!")
+# start background osc event loop
+threading.Thread(target=oscEventLoop).start()
+
+harpaDict = {}
+userInput = input(': ')
+while userInput != 'end':
+    if userInput == 'test':
+        osc_send(oscbuildparse.OSCMessage('/test', None, [userInput]), 'scOSCClient')
+        harpaDict['name'] = harpa.Frequencies().startup('test')
+    userInput = input(': ')
+
+
+finished = True
+
+
+
+
+
+
+# async def event_loop():
+#     print('event loop running')
+#     finished = False
+#     while not finished:
+#         osc_process()
+#         await asyncio.sleep(0.001)
+#     print('exited event loop')
+
+
+# async def ui():
+#     while input(': ') != 'end':
+#         pass
+
+# async def main():
+#     eventLoopTask = asyncio.create_task(event_loop())
+#     uiTask = asyncio.create_task(ui())
+#     await eventLoopTask
+#     event_loop.finished = True
+    
+
+# osc_startup()
+# asyncio.run(main())
+
+
+
+
+
+# harpaDict = {}
+
+# harpaDict['name'] = subprocess.Popen(
+#     ['python', 'instruments/harpa/source/frequencies-test.py', 'oscAdress'],
+#     stdout=subprocess.PIPE,
+#     text=True
+# )
+
+
+
+# output = harpaDict['name'].communicate()[0]
+# print(output)
